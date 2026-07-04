f0323a2692edd1e87a49a385c1a7bd39a3f9d39e
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-07-04 19:43:01 +0900

    Move source into src/ and add package build files

---
+++ default.nix
@@ -0,0 +1,15 @@
+let
+  dev = import ./dev.nix;
+  pkgs-nix = import ./h8x.nix;
+in
+{ pkgs ? pkgs-nix
+, ghc ? dev.compiler-nix-name
+} : pkgs.haskell-nix.cabalProject {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "crypto-srp";
+    src = ./.;
+  };
+  compiler-nix-name = ghc;
+  inherit (dev) index-state;
+}
